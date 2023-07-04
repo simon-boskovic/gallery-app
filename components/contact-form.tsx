7c964eef101958014f0906d3ca998f1463797d19
Simon Boskovic <bosksi@gmail.com>
2023-07-04 18:59:43 +0200

    DD

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -2,18 +2,80 @@
 import { useRef, useState } from "react";
 import styles from "../styles/ContactForm.module.scss";
 
+type Service =
+  | "Parové focení"
+  | "Těhotenské focení"
+  | "Rodiné focení"
+  | "Portréty";
+
 export default function ContactForm() {
   const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const phoneRef = useRef<HTMLInputElement>(null);
+  const messageRef = useRef<HTMLTextAreaElement>(null);
+  const [services, setServices] = useState<Service[]>([]);
 
-  const onSubmit = (event) => {
+  const handleServiceChange = (event) => {
+    const service = event.target.value as Service;
+    if (event.target.checked) {
+      setServices([...services, service]);
+    } else {
+      setServices(services.filter((s) => s !== service));
+    }
+  };
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(nameRef.current?.value);
+
+    if (
+      !nameRef.current?.value ||
+      !emailRef.current?.value ||
+      !phoneRef.current?.value ||
+      !messageRef.current?.value
+    ) {
+      alert("Nebyly vyplněny všechny údaje");
+      return;
+    }
+
+    const formData = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      phone: phoneRef.current.value,
+      message: messageRef.current.value,
+      services: services,
+    };
+
+    try {
+      const response = await fetch(
+        "https://raiderrock.cz/test/index.php?contactForm",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      if (response.ok) {
+        alert("Formulář byl úspěšně odeslán");
+        nameRef.current.value = "";
+        emailRef.current.value = "";
+        phoneRef.current.value = "";
+        messageRef.current.value = "";
+        setServices([]); // Vynulování služeb po odeslání
+      } else {
+        alert("Při odesílání formuláře došlo k chybě");
+      }
+    } catch (error) {
+      console.error("Chyba při odesílání formuláře:", error);
+    }
   };
 
   return (
     <div className={styles["c-form-wrapper"]}>
       <div className={styles["c-form"]}>
-        <form onSubmit={onSubmit}>
+        <form onSubmit={handleSubmit}>
           <div className={styles["c-form-grid"]}>
             <div className={styles["c-grid-item"]}>
               <div className={styles["c-grid-item-label"]}>
@@ -34,10 +96,11 @@ export default function ContactForm() {
               <div className={styles["c-grid-item-label"]}>Email</div>
               <div className={styles["c-grid-item-wrap"]}>
                 <input
+                  ref={emailRef}
                   type="text"
                   className={styles["c-grid-item-wrap"]}
-                  name="name"
-                  id="name"
+                  name="email"
+                  id="email"
                   required
                 />
               </div>
@@ -46,10 +109,11 @@ export default function ContactForm() {
               <div className={styles["c-grid-item-label"]}>Telefon</div>
               <div className={styles["c-grid-item-wrap"]}>
                 <input
+                  ref={phoneRef}
                   type="text"
                   className={styles["c-grid-item-wrap"]}
-                  name="name"
-                  id="name"
+                  name="phone"
+                  id="phone"
                   required
                 />
               </div>
@@ -58,31 +122,56 @@ export default function ContactForm() {
               <div className={styles["c-grid-item-label"]}>Zpráva</div>
               <div className={styles["c-grid-item-wrap"]}>
                 <textarea
+                  ref={messageRef}
                   className={styles["c-grid-item-wrap"]}
                   rows={5}
-                  name="name"
-                  id="name"
+                  name="message"
+                  id="message"
                   required
                 />
               </div>
             </div>
           </div>
           <div className={styles["c-checkbox-wrapper"]}>
-            <div className={styles["c-field-name"]}>Mám zájem o službu: </div>
+            <div className={styles["c-field-name"]}>Mám zájem o službu:</div>
             <div className={styles["c-field-input-wrapper"]}>
-              <input type="checkbox" id="parove_foceni" />
+              <input
+                type="checkbox"
+                id="parove_foceni"
+                value="Parové focení"
+                checked={services.includes("Parové focení")}
+                onChange={handleServiceChange}
+              />
               <label htmlFor="parove_foceni">Parové focení</label>
             </div>
             <div className={styles["c-field-input-wrapper"]}>
-              <input type="checkbox" id="tehotenske_foceni" />
+              <input
+                type="checkbox"
+                id="tehotenske_foceni"
+                value="Těhotenské focení"
+                checked={services.includes("Těhotenské focení")}
+                onChange={handleServiceChange}
+              />
               <label htmlFor="tehotenske_foceni">Těhotenské focení</label>
             </div>
             <div className={styles["c-field-input-wrapper"]}>
-              <input type="checkbox" id="rodine_foceni" />
+              <input
+                type="checkbox"
+                id="rodine_foceni"
+                value="Rodiné focení"
+                checked={services.includes("Rodiné focení")}
+                onChange={handleServiceChange}
+              />
               <label htmlFor="rodine_foceni">Rodiné focení</label>
             </div>
             <div className={styles["c-field-input-wrapper"]}>
-              <input type="checkbox" id="portrety" />
+              <input
+                type="checkbox"
+                id="portrety"
+                value="Portréty"
+                checked={services.includes("Portréty")}
+                onChange={handleServiceChange}
+              />
               <label htmlFor="portrety">Portréty</label>
             </div>
           </div>
--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import navigationStyles from "../styles/Navigation.module.scss";
 import Link from "next/link";
 import { useEffect, useState } from "react";
@@ -15,27 +14,33 @@ export default function Navbar() {
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-  });
+  }, []);
+
+  useEffect(() => {
+    const navbar = document.getElementById("navbar");
+    if (isScrolled) {
+      navbar!.style.top = "0";
+    } else {
+      navbar!.style.top = "40px";
+    }
+  }, [isScrolled]);
+
   return (
-    <div className={`${navigationStyles["c-navigation"]}`}>
+    <div
+      id="navbar"
+      className={`${navigationStyles["c-navigation"]} ${
+        isScrolled ? navigationStyles["c-navigation--scrolled"] : ""
+      }`}
+    >
       <div className={`${navigationStyles["c-nav-container"]}`}>
-        <Link
-          href={"/"}
-          className={`${navigationStyles["c-logo-wrapper"]} ${
-            isScrolled ? navigationStyles["c-logo-wrapper--scrolled"] : ""
-          }`}
-        >
-          <img
-            src={"/images/hero/compressed/images/hero-image.webp"}
-            alt="Image"
-            className={`${navigationStyles["c-logo"]}`}
-          />
-          <div
-            className={`${navigationStyles["c-text-logo"]} ${
-              isScrolled ? navigationStyles["c-text-logo--scrolled"] : ""
-            }`}
-          >
-            HREF
+        <Link href="/" passHref>
+          <div className={`${navigationStyles["c-logo-wrapper"]}`}>
+            <img
+              src="/images/hero/compressed/images/hero-image.webp"
+              alt="Image"
+              className={`${navigationStyles["c-logo"]}`}
+            />
+            <div className={`${navigationStyles["c-text-logo"]}`}>HREF</div>
           </div>
         </Link>
         <div className={`${navigationStyles["c-menu-button-container"]}`}>
@@ -57,37 +62,21 @@ export default function Navbar() {
             </div>
           </div>
           <div
-            className={`${navigationStyles["c-menu-navigation"]}
-        ${isMenuOpen ? "" : navigationStyles["c-menu-navigation--closed"]}
-        `}
+            className={`${navigationStyles["c-menu-navigation"]} ${
+              isMenuOpen ? "" : navigationStyles["c-menu-navigation--closed"]
+            }`}
           >
-            <Link
-              href="/#portfolio"
-              scroll={false}
-              onClick={() => setIsMenuOpen(false)}
-            >
-              <div>Portfolio</div>
+            <Link href="/#portfolio" scroll={false} passHref>
+              <div onClick={() => setIsMenuOpen(false)}>Portfolio</div>
             </Link>
-            <Link
-              href="/#about-me"
-              scroll={false}
-              onClick={() => setIsMenuOpen(false)}
-            >
-              <div>O mě</div>
+            <Link href="/#about-me" scroll={false} passHref>
+              <div onClick={() => setIsMenuOpen(false)}>O mě</div>
             </Link>
-            <Link
-              href="/#services"
-              scroll={false}
-              onClick={() => setIsMenuOpen(false)}
-            >
-              <div>Služby</div>
+            <Link href="/#services" scroll={false} passHref>
+              <div onClick={() => setIsMenuOpen(false)}>Služby</div>
             </Link>
-            <Link
-              href="/#contact"
-              scroll={false}
-              onClick={() => setIsMenuOpen(false)}
-            >
-              <div>Kontakt</div>
+            <Link href="/#contact" scroll={false} passHref>
+              <div onClick={() => setIsMenuOpen(false)}>Kontakt</div>
             </Link>
           </div>
         </div>
