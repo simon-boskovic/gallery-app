019323d38e6fb1720dbbd7912516e199fbff8aa7
Simon Boskovic <bosksi@gmail.com>
2023-07-16 14:20:46 +0200

    DD

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,31 +14,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="76x76"
-          href="/images/layout/meta/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/images/layout/meta/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/images/layout/meta/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/images/layout/meta/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/images/layout/meta/safari-pinned-tab.svg"
-          color="#5bbad5"
-        />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
+        <meta name="msapplication-TileColor" content="#9d0208" />
+        <meta name="theme-color" content="#9d0208" />
       </Head>
       <body
         className={` ${poppins.className}`}
